b4314e3515df99a8a73817c0440b310e6780a881
jproque-dell <jeanpierre.roquesalane@dell.com>
2025-09-09 17:38:58 +0200

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -2,10 +2,10 @@ from zaza.openstack.charm_tests.cinder_backend.tests import CinderBackendTest
 from os import environ
 
 
-class CinderPowerMaxFCTest(CinderBackendTest):
+class CinderPowerMaxTest(CinderBackendTest):
     """Encapsulate cinder-powermax tests."""
 
-    backend_name = 'cinder-powermax-fc'
+    backend_name = 'cinder-powermax'
 
     expected_config_content = {
         'cinder-powermax': {
@@ -14,17 +14,3 @@ class CinderPowerMaxFCTest(CinderBackendTest):
             'san_login': environ['TEST_POWERMAX_SAN_USERNAME'],
             'san_password': environ['TEST_POWERMAX_SAN_PASSWORD']
         }}
-
-class CinderPowerMaxISCSITest(CinderBackendTest):
-    """Encapsulate cinder-powermax tests."""
-
-    backend_name = 'cinder-powermax-iscsi'
-
-    expected_config_content = {
-        'cinder-powermax': {
-            'volume_driver': ['cinder.volume.drivers.dell_emc.powermax.iscsi.PowerMaxISCSIDriver'],
-            'san_ip': environ['TEST_POWERMAX_SAN_IP'],
-            'san_login': environ['TEST_POWERMAX_SAN_USERNAME'],
-            'san_password': environ['TEST_POWERMAX_SAN_PASSWORD']
-        }}
-
